a4b13dc8e11fbe4c75fde609bfc7d0027bbc6e09
wayne0415 <wayne20003106@gmail.com>
2024-09-19 07:19:24 +0800

    123

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -3,6 +3,7 @@ import black_tea_img from "../assets/images/black-tea-img.jpg";
 import black from "../assets/images/blackTea.png";
 import oolong from "../assets/images/oolongTea.png";
 import green from "../assets/images/greenTea.png";
+import FadeInSection from "./FadeInSection";
 
 function Filter() {
     // 定義狀態來追蹤當前選中的茶類別
@@ -27,41 +28,53 @@ function Filter() {
 
     return (
         <section className="filter">
-            <p className="filter-title" id="filter-title">
-                <span>
-                    &emsp;&emsp;我們為您帶來獨特的品茶體驗，
-                    <br />
-                    將東方茶藝的深厚傳統與現代創新技法完美融合。
-                    <br />
-                    每一杯茶都糅合了自然的純淨和細膩的風味，
-                    <br />
-                    傳遞出文化的深度與創意的碰撞。品味傳統，
-                    <br />
-                    探索創新，感受茶飲的無限可能。
-                </span>
-            </p>
+            <FadeInSection effect="fade-in-down">
+                <p className="filter-title" id="filter-title">
+                    <span>
+                        &emsp;&emsp;我們為您帶來獨特的品茶體驗，
+                        <br />
+                        將東方茶藝的深厚傳統與現代創新技法完美融合。
+                        <br />
+                        每一杯茶都糅合了自然的純淨和細膩的風味，
+                        <br />
+                        傳遞出文化的深度與創意的碰撞。品味傳統，
+                        <br />
+                        探索創新，感受茶飲的無限可能。
+                    </span>
+                </p>
+            </FadeInSection>
             <div className="filter-main">
                 <div className="filter-img">
-                    <img src={black_tea_img} alt="black_tea_img" />
+                    <FadeInSection effect="fade-in-right">
+                        <img src={black_tea_img} alt="black_tea_img" />
+                    </FadeInSection>
                 </div>
                 <div className="filter-form">
                     <div className="filter-button">
-                        <button className="filter-btn black-tea" onClick={() => handleFilterClick("black")}>
-                            紅茶
-                        </button>
-                        <button className="filter-btn oolong-tea" onClick={() => handleFilterClick("oolong")}>
-                            烏龍茶
-                        </button>
-                        <button className="filter-btn green-tea" onClick={() => handleFilterClick("green")}>
-                            綠茶
-                        </button>
+                        <FadeInSection effect="fade-in-right">
+                            <button className="filter-btn black-tea" onClick={() => handleFilterClick("black")}>
+                                紅茶
+                            </button>
+                        </FadeInSection>
+                        <FadeInSection effect="fade-in-down">
+                            <button className="filter-btn oolong-tea" onClick={() => handleFilterClick("oolong")}>
+                                烏龍茶
+                            </button>
+                        </FadeInSection>
+                        <FadeInSection effect="fade-in-left">
+                            <button className="filter-btn green-tea" onClick={() => handleFilterClick("green")}>
+                                綠茶
+                            </button>
+                        </FadeInSection>
                     </div>
                     <div id="tea-list">
-                        {teaItems.map((item) => (
-                            <div className="filter-content" key={item.category}>
-                                <img src={item.src} alt={item.alt} />
-                            </div>
-                        ))}
+                        <FadeInSection effect="fade-in-up">
+                            {teaItems.map((item) => (
+                                <div className="filter-content" key={item.category}>
+                                    <img src={item.src} alt={item.alt} />
+                                </div>
+                            ))}
+                        </FadeInSection>
                     </div>
                 </div>
             </div>
