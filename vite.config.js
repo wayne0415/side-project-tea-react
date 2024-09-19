8bb2395b4a415127ce77388356f1a19bc981a40a
wayne0415 <wayne20003106@gmail.com>
2024-09-19 13:28:12 +0800

    123

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "side-project-tea-react",
+    base: "/side-project-tea-react/", // 確保前後都有 "/"
 });
